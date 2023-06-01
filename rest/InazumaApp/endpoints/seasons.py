1396cb568d045f178cec9ea5395b07e408113b6d
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2023-06-01 10:25:16 +0200

    Arreglado problema del endpoint de seasons por id

--- rest/InazumaApp/endpoints/seasons.py
+++ rest/InazumaApp/endpoints/seasons.py
@@ -4,11 +4,11 @@ from InazumaApp.models import Temporada, JugadorClubTemporada
 
 def seasonsByID(request, sid):
     seasons_get = Temporada.objects.get(id=sid)
-    #print(seasons_get)
+    print(seasons_get)
 
     clubes = []
     for i in JugadorClubTemporada.objects.all():
-        if i.club == seasons_get:
+        if i.temporada == seasons_get:
             clubes.append({"nombre": i.club.nombre, "logo": i.club.logo})
 
     return JsonResponse({
